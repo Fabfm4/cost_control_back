ff9d28b7c8b6fe0c7ef553609ec94dbc4ab444c7
Fabian Flores <favian.flone92@gmail.com>
2024-07-02 13:05:11 -0600

    feat(transaction_split): create transaction_split app

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from bank.infrastructure.api import router as bank_router
 from card.infrastructure.api import router as card_router
 from transaction.infrastructure.api import router as transaction_router
+from transaction_split.infrastructure.api import router as transaction_split_router  # noqa
 
 
 app = FastAPI(
@@ -15,3 +16,4 @@ app = FastAPI(
 app.include_router(bank_router)
 app.include_router(card_router)
 app.include_router(transaction_router)
+app.include_router(transaction_split_router)
